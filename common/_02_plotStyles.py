077a7360a5ecd4833f71dfc1d78d5867a96bcd06
Austin Park <austinpark77@gmail.com>
2019-04-03 16:00:17 -0700

    Upload contents

---
+++ common/_02_plotStyles.py
@@ -0,0 +1,10 @@
+### Plot Styles ###
+# Sets up font and layout preferences. Used for consistency.
+import seaborn as sns
+sns.set()
+sns.set_context("talk", rc={"font.size":16,
+                            "axes.titlesize":24,
+                            "axes.labelsize":20})
+
+
+# NOTE: Get general working filename?
