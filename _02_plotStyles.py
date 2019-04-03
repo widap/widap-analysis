aa182e744f52d16eba0c1e19b56003d1ada72978
Austin Park <austinpark77@gmail.com>
2019-04-02 18:13:03 -0700

    Helper functions - initial commit

---
+++ _02_plotStyles.py
@@ -0,0 +1,7 @@
+### Plot Styles ###
+# Sets up font and layout preferences. Used for consistency.
+import seaborn as sns
+sns.set()
+sns.set_context("talk", rc={"font.size":16,
+                            "axes.titlesize":24,
+                            "axes.labelsize":20})
